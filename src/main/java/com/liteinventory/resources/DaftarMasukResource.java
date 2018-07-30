a4a98b12b0f473b9a80b7a2b814cd79ee4037f2c
rudiansen <rudiansen.g@gmail.com>
2018-07-31 00:08:21 +0800

    add new method

--- src/main/java/com/liteinventory/resources/DaftarMasukResource.java
+++ src/main/java/com/liteinventory/resources/DaftarMasukResource.java
@@ -1,12 +1,15 @@
 package com.liteinventory.resources;
 
 import java.util.List;
+import java.util.Optional;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.liteinventory.core.DaftarMasuk;
 import com.liteinventory.db.DaftarMasukDAO;
@@ -33,5 +36,21 @@ public class DaftarMasukResource {
 	@UnitOfWork
 	public List<DaftarMasuk> listDaftarMasuk() {
 		return dmDAO.findAll();
+	}
+	
+	@GET
+	@Path("/{id}")
+	@UnitOfWork
+	public Optional<DaftarMasuk> findById(long id) {
+		return dmDAO.findById(id);
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	@UnitOfWork
+	public Response deleteById(long id) {
+		dmDAO.delete(id);
+		
+		return Response.noContent().build();
 	}
 }
